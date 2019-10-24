d8605205473af7a41c5c48162a41312adce6e804
2dam <2dam@MACINF03.TartangaLH.eus>
2019-10-24 14:05:55 +0200

    DAO of the server part

--- src/server/model/DAO.java
+++ src/server/model/DAO.java
@@ -5,10 +5,92 @@
  */
 package server.model;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import utilities.beans.User;
+
+
+
 /**
  *
  * @author adria
  */
 public class DAO {
+    private Connection con = null;
+    private Statement stmt;
+    private String message;
+    
+    /**
+     * Connection with the Database
+     */
+    private void connect(){
+        try {
+            con = PoolDB.getConnection();
+            stmt = con.createStatement();
+        } catch (SQLException ex) {
+            //TODO EXCEPTION
+        }
+    }
+    
+    /**
+     * Disconnect whit the database
+     */
+    private void disconnect(){
+        try{
+            if(stmt != null)
+                stmt.close();
+            if(con != null)
+                con.close();
+        } catch(SQLException e){
+            //TODO EXCEPTION
+        } finally{
+            PoolDB.liberarConexion();
+        }
+    }
+    
+    
+    //ClASS METHODS
+    
+    public String getMessage(){
+        //Method that return the final result of the methods of DAO
+        return this.message;
+    }
+    
+   //DB METHODS
+    
+    /**
+     * Method to LogIn the application
+     * @param user
+     * @return user with the result
+     */
+    public User logIn(User user){
+        int result = 0;
+        this.connect();
+        
+        
+        
+        
+        
+        return user;
+    }
+    
+    /**
+     * Method to register a new user
+     * @param user
+     * @return user
+     */
+    public User signUp(User user){
+        
+        return user;
+    }
+    
+    /**
+     * Method to SignOut the application
+     * @param user 
+     */
+    public void signOut(User user){
+        
+    }
     
 }
